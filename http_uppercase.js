06793e1fb5ea92ec0ae16720638a0666226c7faf
郑俊鑫 <18810544291@163.com>
2017-05-30 09:17:53 +0800

    http_uppercase

---
+++ http_uppercase.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const port = process.argv[2]
+const map = require('through2-map')
+
+const server = http.createServer((req, res) => {
+    req.pipe(map(chunk => {
+        return chunk.toString().toUpperCase()
+    })).pipe(res)
+})
+
+
+server.listen(port)
