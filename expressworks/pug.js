c244e3c0860eb6304fd88eb8a7f4822e63cfecf2
郑俊鑫 <18810544291@163.com>
2017-05-31 15:10:01 +0800

    pug

---
+++ expressworks/pug.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.argv[2]
+const viewPath = process.argv[3]
+
+app.set('views', viewPath)
+app.set('view engine', 'pug')
+app.get('/home', (req, res) => {
+    res.render('index', {date: new Date().toDateString()})
+})
+app.listen(port)
