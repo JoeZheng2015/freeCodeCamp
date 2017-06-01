29699ba95af70b60bc6013ccf7b5d4c42e843ed9
郑俊鑫 <18810544291@163.com>
2017-06-01 22:05:56 +0800

    使用 req.params 获取 param，使用 crypto 生成SHA1

---
+++ expressworks/hash_param.js
@@ -0,0 +1,12 @@
+const crypto = require('crypto')
+const express = require('express')
+const app = express()
+
+app.put('/message/:id', (req, res) => {
+    const id = req.params.id
+    const hash = crypto.createHash('sha1')
+        .update(new Date().toDateString() + id)
+        .digest('hex')
+    res.send(hash)
+})
+app.listen(process.argv[2])
