2f6ea6f7a53116e32713b85093f7b12455e696a9
郑俊鑫 <18810544291@163.com>
2017-06-09 21:09:34 +0800

    使用 stackoverflow 推荐的方式获取客户端 ip

--- whoami/index.js
+++ whoami/index.js
@@ -8,9 +8,14 @@ const softwarePattern = /Mozilla\/\d.?\d \((.+?)\) .*/
 app.get('/', (req, res) => {
     const lang = languagePattern.exec(req.headers['accept-language'])
     const ua = softwarePattern.exec(req.headers['user-agent'])
+    const ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress
 
+    // req.ip 也可以，但 stackoverflow 上更多使用 x-forwarded-for
     res.send({
-        ipaddress: req.ip,
+        ipaddress: ip,
         language: lang ? lang[0] : '',
         software: ua ? ua[1] : '',
     })
