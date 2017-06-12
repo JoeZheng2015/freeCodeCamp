afd3b9edc3fbffd3d3879238255789777f09882c
郑俊鑫 <18810544291@163.com>
2017-06-12 22:01:15 +0800

    使用 express.static 来返回静态的 index.html 文件；使用 formData 对象通过 fetch 来上传文件

--- file_metadata/index.js
+++ file_metadata/index.js
@@ -1,19 +1,15 @@
 const express = require('express')
 const multer = require('multer')
-const fs = require('fs')
 const path = require('path')
 
 const upload = multer()
 const app = express()
 const port = 8080
 
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
 
-app.get('/', (req, res) => {
-    res.send(html)
-})
+app.use(express.static(path.resolve(__dirname)))
 
-app.post('/profile', upload.single('file'), (req, res) => {
+app.post('/upload', upload.single('file'), (req, res) => {
     if (req.file) {
         res.send({
             size: req.file.size,
